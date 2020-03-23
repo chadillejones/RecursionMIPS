0d15a698e7792991223e666ae049c364e3a71d10
chadillejones <chadille.jones@bison.howard.edu>
2020-03-23 01:36:25 -0400

    initial commits

--- recursion.s
+++ recursion.s
@@ -4,3 +4,11 @@
 .data  #data declaration section
 user_input: .space 2000 #creating space for the users input
 invalid: .asciiz "Invalid input"
+
+.text #Assembly language instruction
+
+main: 
+
+	li $v0, 8 #accepts user input
+	la $a0, user_input
+	li $a1, 102 #specify the length the user can input
